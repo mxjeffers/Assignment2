9b236b95c031d25727dba29692769a1dad2e11eb
mxjeffers <30639610+mxjeffers@users.noreply.github.com>
2020-04-16 10:31:19 -0600

    Updated comments

--- balance.py
+++ balance.py
@@ -1,3 +1,5 @@
+# Malcolm Jeffers
+# CS261 Assignment 2
 # balance.py
 # ===================================================
 # Using a stack to check for unbalanced parentheses
@@ -20,6 +22,10 @@ def is_balanced(input_string):
         "}": "{"
     }
     # iterate over each character in the string
+    # Search if the character is in open list. If it is then
+    # place it on the stack. If a character is in the closing list,
+    # call the item at the top of the stack. If it corresponds. Then
+    # remove it from the stack.
     for i in input_string:
         if i in open:
             stack.append(i)
@@ -29,18 +35,18 @@ def is_balanced(input_string):
                     del stack[-1]
             else:
                 return False
+    # If the stack is empty at the end then return True.
     if stack == []:
         return True
     else:
         return False
 
-        return False
 
 
 if __name__ == '__main__':
     # get input string
-    #_input_string = sys.argv[1]  # DO NOT MODIFY
-    _input_string = "4[56]"
+    _input_string = sys.argv[1]  # DO NOT MODIFY
+    #_input_string = "4[56]"
     balanced = is_balanced(_input_string)
 
     if balanced:
